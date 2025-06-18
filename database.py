7fff494898711e6863ff61b15c8f4e319601cedb
ledesma-ivan <ivanarielledesma@gmail.com>
2025-06-18 16:27:54 -0300

    xD

--- database.py
+++ database.py
@@ -14,3 +14,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 #  Creamos la clase base que nos sirve para definir el modelado de datos.
 Base = declarative_base()
+
+# Sirve pa' agarrar la conexión a la base de datos, que después la usás tranqui en cualquier parte
+def get_db():
+    db = SessionLocal()  # Abrimos la puerta pa' la base
+    try:
+        yield db  # Te tiro la sesión activa, usala bien
+    finally:
+        db.close()  # Cierro la puerta pa' que no quede nada abierto y no jodamos
